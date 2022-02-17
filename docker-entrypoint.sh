294f47d1f1e0a368918fed6afc5edc082d6af6a9
TAKANO Mitsuhiro <takano32@gmail.com>
2022-02-17 21:24:43 +0900

    Build Linux v5.16
    Update Linux version from v5.8 to v5.16

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -eux
 cd /build-kernel/linux
 
 git fetch origin
-git checkout -b tag/v5.8 refs/tags/v5.8
+git checkout -b tag/v5.16 refs/tags/v5.16
 
 GENERIC_CONFIG_URL=https://kernel.ubuntu.com/~kernel-ppa/config/focal/linux/5.4.0-49.53/amd64-config.flavour.generic
 curl -L $GENERIC_CONFIG_URL > /build-kernel/build/.config
@@ -13,11 +13,11 @@ curl -L $GENERIC_CONFIG_URL > /build-kernel/build/.config
 
 make O=/build-kernel/build/ olddefconfig
 
-time make -j9         O=/build-kernel/build/ LOCALVERSION=-stock
+time make -j16            O=/build-kernel/build/ LOCALVERSION=-stock
 
-time make -j9 modules O=/build-kernel/build/ LOCALVERSION=-stock
+time make -j16 modules    O=/build-kernel/build/ LOCALVERSION=-stock
 
-time make bindeb-pkg  O=/build-kernel/build/ LOCALVERSION=-stock
+time make -j16 bindeb-pkg O=/build-kernel/build/ LOCALVERSION=-stock
 
 cd /build-kernel
 cp *.deb dpkg
