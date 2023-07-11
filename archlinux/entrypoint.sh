12b92b42450ef8e195698e076f58bfb0b7c40f9f
TAKANO Mitsuhiro <takano32@gmail.com>
2023-07-12 00:12:34 +0900

    Arch Linux: Set number of jobs to `MAKEFLAGS`

--- archlinux/entrypoint.sh
+++ archlinux/entrypoint.sh
@@ -2,11 +2,10 @@
 set -eux
 
 BUILD_DIR=/build-kernel/build
-SUDO="sudo -u takano32"
+SUDO="sudo -Eu takano32"
 
 mkdir -p $BUILD_DIR
 chown -R takano32:takano32 $BUILD_DIR
-
 cd $BUILD_DIR
 $SUDO asp update linux
 $SUDO asp export linux
@@ -18,6 +17,10 @@ $SUDO git config --global http.postBuffer 524288000
 while :; do $SUDO makepkg -o --skippgpcheck && break || sleep 5; done
 
 # `makepkg` in `$BUILD_DIR/linux`
+JOBS=$(getconf _NPROCESSORS_ONLN)
+JOBS=$(expr "$JOBS" + "$JOBS")
+MAKEFLAGS="-j$(JOBS)"
+export MAKEFLAGS
 $SUDO makepkg --skippgpcheck
 
 cd $BUILD_DIR
